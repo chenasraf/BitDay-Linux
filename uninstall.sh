d153efdbabc73ba87b102d4e30079afc44cd4cac
Chen Asraf <chen@asraf.me>
2013-12-29 15:45:45 +0200

    Comments, strings, separators

--- uninstall.sh
+++ uninstall.sh
@@ -14,16 +14,20 @@ echo
 
 read -p "Are you sure that's what you want? [y/n] " yn
 case $yn in
-	[Yy]* )
+	[Yy]*)
 		echo
 		echo "Removing tasks..."
 
-		line="${pwd}/update.sh"
-		crontab -l | grep -v "$line1" | crontab -
+		line="${pwd}/update.sh" # This should be enough to identify the lines in cron.
+		crontab -l | grep -v "$line1" | crontab - # Re-output any lines that are NOT the previous line (effectively removing it) from crontab
 
 		if [ -f /etc/pm/sleep.d/RotatingWallpaper.sh ]; then
 			echo "We need root permissions to delete '/etc/pm/sleep.d/RotatingWallpaper.sh': "
 			sudo -k sh -c "rm /etc/pm/sleep.d/RotatingWallpaper.sh"
+			if [[ $? -ne 0 ]]; then # If there was a problem deleting (user failed/refused to give root?)
+				echo "There was a problem deleting the unsuspend file, please delete it manually:"
+				echo "    sudo rm /etc/pm/sleep.d/RotatingWallpaper.sh"
+			fi
 		fi
 		;;
 esac
@@ -31,9 +35,7 @@ echo
 
 read -p "Delete this directory and its files? [y/n] " yn
 case $yn in
-	[Yy]* )
-		rm -R $pwd
-		;;
+	[Yy]*) rm -R $pwd;;
 esac
 echo
 
--- update.sh
+++ update.sh
@@ -33,19 +33,20 @@ timing=(
 
 hour=`date +%H`
 
+
+# Different desktop environment implementations
 case $XDG_CURRENT_DESKTOP in
 	Mint|Mate) setcmd="gsettings set org.mate.background picture-uri";;
 	Cinnamon) setcmd="gsettings set org.cinnamon.background picture-uri";;	
-	*) setcmd="feh --bg-fill";;
+	*) setcmd="gsettings set org.gnome.desktop.background picture-uri";; # GNOME/Unity, default
 esac
-if [[ -z $XDG_CURRENT_DESKTOP ]]; then
+if [[ -z $XDG_CURRENT_DESKTOP ]]; then # Fallback for i3
 	case $DESKTOP_SESSION in
 		i3) setcmd="feh --bg-fill"
 	esac
 fi
 
-for i in {7..0..-1}
-do 
+for i in {7..0..-1}; do # Loop backwards through the wallpapers
     if [[ $hour -ge ${timing[i]} ]]; then
         $setcmd file://$DIR/${files[i]}
         echo "Wallpaper set to ${files[i]}"
@@ -53,5 +54,6 @@ do
     fi
 done
 
+# Fallback at last wallpaper if time is not relevant
 $setcmd file://$DIR/${files[7]}
 echo "Wallpaper set to ${files[7]}"
