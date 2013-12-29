95faaf29b20e54ae409614b29a1f1665602a1fd8
Chen Asraf <chen@asraf.me>
2013-12-29 12:01:35 +0200

    Converted .zip to .tar.gz for better compatibility

--- install.sh
+++ install.sh
@@ -11,11 +11,57 @@ echo $sep
 echo
 
 
-echo "* Changing permissions... [1/2]"
+echo "* Changing permissions... [1/5]"
 chmod +x update.sh
 echo
 
-echo "* Creating cron jobs... [2/2]"
+echo "* Select a resolution (files will be downloaded): [2/5]"
+echo "    1) 1280x720"
+echo "    2) 1280x800"
+echo "    3) 1366x768"
+echo "    4) 1440x900"
+echo "    5) 1600x900"
+echo "    6) 1680x1050"
+echo "    7) 1920x1080"
+echo "    8) 1920x1200"
+echo "    9) 2560x1440"
+echo "    10) 2560x1600"
+echo "    11) 2880x1800"
+echo
+
+input=
+while [[ "$input" = "" ]]; do
+	read -p "    Type a number (1-11): " input
+done
+
+case $input in
+	1) file="1280x720";;
+	2) file="1280x800";;
+	3) file="1366x768";;
+	4) file="1440x900";;
+	5) file="1600x900";;
+	6) file="1680x1050";;
+	7) file="1920x1080";;
+	8) file="1920x1200";;
+	9) file="2560x1440";;
+	10) file="2560x1600";;
+	11) file="2880x1800";;
+esac
+
+if [[ -z $file ]]; then
+	file=2880x1800
+fi
+download="https://github.com/chenasraf/8BitDay-Linux/raw/master/tars/BitDay-$file.zip"
+
+echo "* Downloading wallpapers, please wait... [3/5]"
+echo
+wget $download
+echo
+echo "* Extracting zip... [4/5]"
+tar xvf "BitDay-$file.zip" --wildcards '*.png'
+echo "Done."
+
+echo "* Creating cron jobs... [5/5]"
 echo
 
 read -p "Create a cron job every hour? [y/n] " yn
